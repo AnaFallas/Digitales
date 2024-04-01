24288e7e1521688a9ee3a281044d35b84c9381d3
AnaFallas <fallas.ana03@gmail.com>
2024-03-31 22:12:46 -0600

    control actualizacion

--- ControlUnit.sv
+++ ControlUnit.sv
@@ -9,21 +9,21 @@ module Control_Unit (
     output reg RegWrite
 );
 
-    reg [8:0] outcome;
+    reg [7:0] outcome;
 
     always_comb begin
-        {AluSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch} = outcome[8:2];
+        {AluSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch} = outcome[7:2];
         Aluop = outcome[1:0];
     end
 
     always_ff @(OpCode) begin
         case(OpCode)
-            7'b1xx0101x: outcome = 9'b000100010;
-            7'b1111100: outcome = 9'b011110000;
-            7'b1111100: outcome = 9'b110001000;
-            7'b1011010: outcome = 9'b101000101;
-            7'b0000000: outcome = 9'b000000000;
-            default: outcome = 9'b0; // Default 
+            7'b0110011: outcome = 8'b00100010;  //tipo r
+            7'b0000011: outcome = 8'b11110000;  // load
+            7'b0100011: outcome = 8'b10001000;  //store
+            7'b1100011: outcome = 8'b00000101;  //branch
+            7'b0000000: outcome = 8'b000000000;
+            default: outcome = 8'b0; // Default 
         endcase
     end
 endmodule
